6c81893c404861ef0b8f5e887f22ba942c141e57
ada2 <87185550+2ADA2@users.noreply.github.com>
2024-06-03 11:42:25 +0300

    nbiot collector v0.9.4.2

--- src/functions/dataToString.js
+++ src/functions/dataToString.js
@@ -1,11 +1,11 @@
 export const dataToString = (data) => {
     if(isNaN(data.getMonth())) return dataToString(new Date())
     return (
-        data.getFullYear() +
+        (data.getFullYear()>1999) ? data.getMonth() : "2024" +
         "-" +
-        (data.getMonth() + 1) +
+        ((data.getMonth()>9) ? data.getMonth() : "0" + data.getMonth()) +
         "-" +
-        data.getDate() +
+        ((data.getDate()>9) ? data.getDate() : "0" + data.getDate()) +
         "T" +
         ((data.getHours()>9) ? data.getHours() : "0" + data.getHours()) +
         ":" +
